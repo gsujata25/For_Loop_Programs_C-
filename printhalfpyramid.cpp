7376fce9673e36352776718f99010a34d963a0e3
gsujata25 <phd2201201005@iiti.ac.in>
2025-08-26 15:03:53 +0530

    Update printhalfpyramid.cpp

--- printhalfpyramid.cpp
+++ printhalfpyramid.cpp
@@ -1,4 +1,13 @@
-/** Write a program to print half pyramid using * */
+/** Write a program to print half pyramid using 
+
+Input: 3
+
+Output:
+* 
+* * 
+* * * 
+
+* */
 #include <iostream>
 using namespace std;
 
