6626e4e818d688739d0778a019b423ade109fd25
gsujata25 <phd2201201005@iiti.ac.in>
2025-08-26 14:40:59 +0530

    Update printhalfpyramidnum.cpp

--- printhalfpyramidnum.cpp
+++ printhalfpyramidnum.cpp
@@ -1,27 +1,19 @@
 #include <iostream>
 using namespace std;
 
-/*
-Write a program to print pattern below
-
-1 
-2 3 
-4 5 6 
-7 8 9 10 
-
-*/
-
 int main()
 {
-    int n = 4;
+    int n;
+    cout << "Enter number of rows: ";
+    cin >> n;
+
     int num = 1;
     for (int i = 1; i <= n; i++)
     {
-        // numbers
         for (int j = 1; j <= i; j++)
         {
             cout << num << " ";
-            num+=1;
+            num++;
         }
         cout << endl;
     }
